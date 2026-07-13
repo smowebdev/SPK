ac1eff7a58a38f46696bf838a317aac2251a0bec
Huỳnh Nguyễn <107023404+nguyenxuanhuynh2003@users.noreply.github.com>
2026-07-13 17:42:50 +0700

    Upcode : Application Process & Timeline

--- assets/js/main.js
+++ assets/js/main.js
@@ -159,6 +159,31 @@ function main() {
 			}
 		});
 
+		// passport fees
+		var $tabs = $('.passport-fees__tab-input');
+		var $notes = $('.passport-fees__note');
+
+		function showNote(target) {
+			$notes.stop(true, true).slideUp(300);
+			$notes
+				.filter('[data-note="' + target + '"]')
+				.stop(true, true)
+				.slideDown(300);
+		}
+
+		$tabs.on('change', function () {
+			if (!$(this).is(':checked')) {
+				$(this).prop('checked', true);
+				return;
+			}
+
+			$tabs.not(this).prop('checked', false);
+			showNote($(this).data('tab'));
+		});
+
+		$notes.hide();
+		showNote($tabs.filter(':checked').data('tab'));
+
 	}());
 }
 main();
