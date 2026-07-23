da5daeb38e85a56e21dfe9b0facee58eb768d513
nac200220 <161002756+nac200220@users.noreply.github.com>
2026-07-23 17:49:18 +0700

    Update

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,83 +1,104 @@
-$(document).ready(function() {
-    var $owl = $(".category-filter__list").owlCarousel({
+$(document).ready(function () {
+  var $owl = $(".category-filter__list").owlCarousel({
+    loop: false,
+    margin: 16,
+    nav: true,
+    dots: false,
+    autoWidth: true,
+    navText: ["<span>&#10094;</span>", "<span>&#10095;</span>"],
+  });
+
+  $(".filter-btn").on("click", function () {
+    $(".filter-btn").removeClass("active");
+    $(this).addClass("active");
+    $owl.trigger("refresh.owl.carousel");
+
+    var filterValue = $(this).attr("data-filter");
+
+    if (filterValue === "all") {
+      $(".faq-item").fadeIn(300);
+    } else {
+      $(".faq-item").each(function () {
+        if ($(this).attr("data-category") === filterValue) {
+          $(this).fadeIn(300);
+        } else {
+          $(this).fadeOut(300);
+        }
+      });
+    }
+  });
+
+  $(".faq-header").on("click", function () {
+    var $item = $(this).closest(".faq-item");
+
+    var $parentContainer = $item.parent();
+
+    var $content = $item.find(".faq-content");
+
+    $parentContainer
+      .find(".faq-item")
+      .not($item)
+      .find(".faq-content")
+      .slideUp(300);
+    $parentContainer
+      .find(".faq-item")
+      .not($item)
+      .find(".faq-header")
+      .removeClass("active");
+
+    $content.slideToggle(300);
+    $(this).toggleClass("active");
+  });
+
+  $(window).on("resize", function () {
+    clearTimeout(window.resizeTimer);
+    window.resizeTimer = setTimeout(function () {
+      $(".category-filter__list").trigger("destroy.owl.carousel");
+      $(".category-filter__list").owlCarousel({
         loop: false,
         margin: 16,
         nav: true,
         dots: false,
         autoWidth: true,
         navText: ["<span>&#10094;</span>", "<span>&#10095;</span>"],
-    });
+      });
+    }, 300);
+  });
 
-    $(".filter-btn").on("click", function() {
-        $(".filter-btn").removeClass("active");
-        $(this).addClass("active");
-        $owl.trigger("refresh.owl.carousel");
+  const $modal = $(".team-modal");
+  const $modalBody = $(".team-modal__body");
 
-        var filterValue = $(this).attr("data-filter");
+  $(".team-card__link").on("click", function () {
+    const bioId = $(this).data("bio");
+    const bioContent = $("#" + bioId).html();
 
-        if (filterValue === "all") {
-            $(".faq-item").fadeIn(300);
-        } else {
-            $(".faq-item").each(function() {
-                if ($(this).attr("data-category") === filterValue) {
-                    $(this).fadeIn(300);
-                } else {
-                    $(this).fadeOut(300);
-                }
-            });
-        }
-    });
+    $modalBody.html(bioContent);
 
-    $(".faq-header").on("click", function() {
-        var $item = $(this).closest(".faq-item");
+    $modal.addClass("active");
+  });
 
-        var $parentContainer = $item.parent();
+  $(".team-modal__close, .team-modal__overlay").on("click", function () {
+    $modal.removeClass("active");
+  });
 
-        var $content = $item.find(".faq-content");
+  // License Modal
+  const $licenseModal = $(".license-modal");
+  const $licenseModalImage = $(".license-modal img");
 
-        $parentContainer
-            .find(".faq-item")
-            .not($item)
-            .find(".faq-content")
-            .slideUp(300);
-        $parentContainer
-            .find(".faq-item")
-            .not($item)
-            .find(".faq-header")
-            .removeClass("active");
+  $(".partner-benefits__license-caption").on("click", function (e) {
+    e.preventDefault();
 
-        $content.slideToggle(300);
-        $(this).toggleClass("active");
-    });
+    const $card = $(this).closest(".partner-benefits__license");
+    const $img = $card.find(".partner-benefits__license-image img");
 
-    $(window).on("resize", function() {
-        clearTimeout(window.resizeTimer);
-        window.resizeTimer = setTimeout(function() {
-            $(".category-filter__list").trigger("destroy.owl.carousel");
-            $(".category-filter__list").owlCarousel({
-                loop: false,
-                margin: 16,
-                nav: true,
-                dots: false,
-                autoWidth: true,
-                navText: ["<span>&#10094;</span>", "<span>&#10095;</span>"],
-            });
-        }, 300);
-    });
+    $licenseModalImage
+      .attr("src", $img.attr("src"))
+      .attr("alt", $img.attr("alt"));
 
-    const $modal = $(".team-modal");
-    const $modalBody = $(".team-modal__body");
+    $licenseModal.addClass("active");
+  });
 
-    $(".team-card__link").on("click", function() {
-        const bioId = $(this).data("bio");
-        const bioContent = $("#" + bioId).html();
-
-        $modalBody.html(bioContent);
-
-        $modal.addClass("active");
-    });
-
-    $(".team-modal__close, .team-modal__overlay").on("click", function() {
-        $modal.removeClass("active");
-    });
+  $(".license-modal__close, .license-modal__overlay").on("click", function () {
+    $licenseModal.removeClass("active");
+  });
 });
